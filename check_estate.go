b66fa34f20e6198fbab53eeb444d7b68ded607f5
Georgy Buranov <gburanov@gmail.com>
2017-11-20 22:21:19 +0100

    try to change authtorization

--- check_estate.go
+++ check_estate.go
@@ -5,19 +5,27 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/mrjones/oauth"
 )
 
 func callAPI() error {
 	url := "https://rest.immobilienscout24.de/restapi/api/search/v1.0/search/radius?realestatetype=ApartmentRent&geocoordinates=52.512303;13.431191;1"
 
-	client := &http.Client{}
+	username := os.Getenv("IM24_USERNAME")
+	password := os.Getenv("IM24_PASSWORD")
+	consumer := oauth.NewConsumer(username, password, oauth.ServiceProvider{})
+	accessToken := &oauth.AccessToken{}
+	client, err := consumer.MakeHttpClient(accessToken)
+	if err != nil {
+		return err
+	}
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return err
 	}
-	username := os.Getenv("IM24_USERNAME")
-	password := os.Getenv("IM24_PASSWORD")
-	req.SetBasicAuth(username, password)
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
@@ -28,5 +36,6 @@ func callAPI() error {
 	}
 	s := string(bodyText)
 	fmt.Println(s)
+
 	return nil
 }
